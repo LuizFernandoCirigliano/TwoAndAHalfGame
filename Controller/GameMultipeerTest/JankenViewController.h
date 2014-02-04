504e9c19b0310ef59b92164f66c4f916531e96a3
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2014-02-04 11:44:14 -0200

    Add JankenViewController

---
+++ Controller/GameMultipeerTest/JankenViewController.h
@@ -0,0 +1,13 @@
+//
+//  JankenViewController.h
+//  GameMultipeerTest
+//
+//  Created by Francesco Perrotti-Garcia on 2/4/14.
+//  Copyright (c) 2014 Luiz Fernando 2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JankenViewController : UIViewController
+
+@end
---
+++ Controller/GameMultipeerTest/JankenViewController.m
@@ -0,0 +1,55 @@
+//
+//  JankenViewController.m
+//  GameMultipeerTest
+//
+//  Created by Francesco Perrotti-Garcia on 2/4/14.
+//  Copyright (c) 2014 Luiz Fernando 2. All rights reserved.
+//
+
+#import "JankenViewController.h"
+
+@interface JankenViewController ()
+
+@end
+
+@implementation JankenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)click:(UIButton *)sender
+{
+    if ([sender.titleLabel.text  isEqualToString: @"Rock"])
+    {
+        
+    }
+    else if ([sender.titleLabel.text isEqualToString:@"Paper"])
+    {
+        
+    }
+    else if ([sender.titleLabel.text isEqualToString:@"Scissors"])
+    {
+        
+    }
+}
+
+@end
