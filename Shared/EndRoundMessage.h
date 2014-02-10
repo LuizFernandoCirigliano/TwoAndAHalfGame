cde7a1c6f614799bf7c777d51b69038d9e0c3156
Luiz Fernando <lf.villela@gmail.com>
2014-02-10 16:43:04 -0200

    Trying to fix merge

---
+++ Shared/EndRoundMessage.h
@@ -0,0 +1,15 @@
+//
+//  EndRound.h
+//  GestureShenanigans
+//
+//  Created by Luiz Fernando 2 on 11/22/13.
+//  Copyright (c) 2013 Luiz Fernando 2. All rights reserved.
+//
+
+#import "NewMessage.h"
+
+@interface EndRoundMessage : NewMessage
+
+- (NSData *)archiveData;
+
+@end
